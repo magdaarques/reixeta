080e721621166e3046e9014e55ca4378ea6eb473
magdaarques <magdaarques@gmail.com>
2025-12-09 02:00:26 +0100

    Add files via upload

---
+++ js/gradient.js
@@ -0,0 +1,41 @@
+function createAllGradients() {
+  let colStart = color(255, 0, 0);
+  let colEnd = color(255, 255, 0);
+
+  imgAGrad = createGradientImage(imgA, colStart, colEnd);
+  imgBGrad = createGradientImage(imgB, colStart, colEnd);
+  imgCGrad = createGradientImage(imgC, colStart, colEnd);
+  imgDGrad = createGradientImage(imgD, colStart, colEnd);
+  imgEGrad = createGradientImage(imgE, colStart, colEnd);
+  imgFGrad = createGradientImage(imgF, colStart, colEnd);
+  imgGGrad = createGradientImage(imgG, colStart, colEnd);
+  imgHGrad = createGradientImage(imgH, colStart, colEnd);
+  imgIGrad = createGradientImage(imgI, colStart, colEnd);
+  imgJGrad = createGradientImage(imgJ, colStart, colEnd);
+  imgKGrad = createGradientImage(imgK, colStart, colEnd);
+  imgLGrad = createGradientImage(imgL, colStart, colEnd);
+  imgMGrad = createGradientImage(imgM, colStart, colEnd);
+  imgNGrad = createGradientImage(imgN, colStart, colEnd);
+  imgOGrad = createGradientImage(imgO, colStart, colEnd);
+  imgPGrad = createGradientImage(imgP, colStart, colEnd);
+  imgQGrad = createGradientImage(imgQ, colStart, colEnd);
+  imgRGrad = createGradientImage(imgR, colStart, colEnd);
+  imgSGrad = createGradientImage(imgS, colStart, colEnd);
+  imgTGrad = createGradientImage(imgT, colStart, colEnd);
+  imgUGrad = createGradientImage(imgU, colStart, colEnd);
+  imgVGrad = createGradientImage(imgV, colStart, colEnd);
+  imgWGrad = createGradientImage(imgW, colStart, colEnd);
+  imgXGrad = createGradientImage(imgX, colStart, colEnd);
+  imgYGrad = createGradientImage(imgY, colStart, colEnd);
+  imgZGrad = createGradientImage(imgZ, colStart, colEnd);
+
+  imgA1Grad = createGradientImage(imgA1, colStart, colEnd);
+  imgE1Grad = createGradientImage(imgE1, colStart, colEnd);
+  imgE2Grad = createGradientImage(imgE2, colStart, colEnd);
+  imgI1Grad = createGradientImage(imgI1, colStart, colEnd);
+  imgO1Grad = createGradientImage(imgO1, colStart, colEnd);
+  imgO2Grad = createGradientImage(imgO2, colStart, colEnd);
+  imgU1Grad = createGradientImage(imgU1, colStart, colEnd);
+
+  imgReixetaGrad = createGradientImage(imgReixeta, colStart, colEnd);
+}
